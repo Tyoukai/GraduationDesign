432824dc111d8caf4527355cd19543d5f57226ab
Tyoukai <zhang_k_w@163.com>
2019-02-22 16:06:11 +0800

    修改了紧急指数的计算方法，并监控了最新的实验队列情况

--- src/main/java/algorithm1/Monitor.java
+++ src/main/java/algorithm1/Monitor.java
@@ -25,65 +25,56 @@ public class Monitor implements Runnable{
         MessageQueue q2 = queues.get(1);
         MessageQueue q3 = queues.get(2);
         MessageQueue q4 = queues.get(3);
-        MessageQueue q5 = queues.get(4);
         File file1 = new File("q1.txt"); // src\main\resources\q1.txt
         File file2 = new File("q2.txt");
         File file3 = new File("q3.txt");
         File file4 = new File("q4.txt");
-        File file5 = new File("q5.txt");
         FileWriter fw1 = null;
         FileWriter fw2 = null;
         FileWriter fw3 = null;
         FileWriter fw4 = null;
-        FileWriter fw5 = null;
 
         try {
             fw1 = new FileWriter(file1.getName(), true);
             fw2 = new FileWriter(file2.getName(), true);
             fw3 = new FileWriter(file3.getName(), true);
             fw4 = new FileWriter(file4.getName(), true);
-            fw5 = new FileWriter(file5.getName(), true);
 
             long sTime = System.currentTimeMillis();
             long eTime = System.currentTimeMillis();
             int flag = 0;
-            while(eTime - sTime < 100000) {
+            while(eTime - sTime < 90000) {
                 if(eTime -sTime >= 30000 && flag == 0) {
-                    q1.setNumber(5000);
-                    q2.setNumber(9000);
-                    q3.setNumber(4000);
-                    q4.setNumber(10000);
-                    q5.setNumber(10000);
+//                    q1.setNumber(5000);
+//                    q2.setNumber(9000);
+//                    q3.setNumber(4000);
+//                    q4.setNumber(10000);
                     flag = 1;
                 }
                 Thread.sleep(1000);
-                double q1Length = q1.getQij1();
-                double q1R = q1.getRij1();
-                double q2Length = q2.getQij1();
-                double q2R = q2.getRij1();
-                double q3Length = q3.getQij1();
-                double q3R = q3.getRij1();
-                double q4Length = q4.getQij1();
-                double q4R = q4.getRij1();
-                double q5Length = q5.getQij1();
-                double q5R = q5.getRij1();
+                double q1Length = q1.getLength();
+                double q1W = q1.getW();
+                double q2Length = q2.getLength();
+                double q2W = q2.getW();
+                double q3Length = q3.getLength();
+                double q3W = q3.getW();
+                double q4Length = q4.getLength();
+                double q4W = q4.getW();
 
-                fw1.write(q1Length + "____" + q1.getRij1() + "____" + (q1Length / q1R) + "\r\n");
-                fw2.write(q2Length + "____" + q2.getRij1() + "____" + (q2Length / q2R) + "\r\n");
-                fw3.write(q3Length + "____" + q3.getRij1() + "____" + (q3Length / q3R) + "\r\n");
-                fw4.write(q4Length + "____" + q4.getRij1() + "____" + (q4Length / q4R) + "\r\n");
-                fw5.write(q5Length + "____" + q5.getRij1() + "____" + (q5Length / q5R) + "\r\n");
+
+                fw1.write(q1Length + "____" + q1W + "____" + (q1Length / q1W) + "\r\n");
+                fw2.write(q2Length + "____" + q2W + "____" + (q2Length / q2W) + "\r\n");
+                fw3.write(q3Length + "____" + q3W + "____" + (q3Length / q3W) + "\r\n");
+                fw4.write(q4Length + "____" + q4W + "____" + (q4Length / q4W) + "\r\n");
 
                 System.out.println("q1长度                    到达速率                    拥塞指数");
-                System.out.println(q1Length + "                    " + q1R + "                    " + (q1Length / q1R));
+                System.out.println(q1Length + "                    " + q1.getRij1() + "                    " + (q1Length / q1W));
                 System.out.println("q2长度                    到达速率                    拥塞指数");
-                System.out.println(q2Length + "                    " + q2R + "                    " + (q2Length / q2R));
+                System.out.println(q2Length + "                    " + q2.getRij1() + "                    " + (q2Length / q2W));
                 System.out.println("q3长度                    到达速率                    拥塞指数");
-                System.out.println(q3Length + "                    " + q3R + "                    " + (q3Length / q3R));
+                System.out.println(q3Length + "                    " + q3.getRij1() + "                    " + (q3Length / q3W));
                 System.out.println("q4长度                    到达速率                    拥塞指数");
-                System.out.println(q4Length + "                    " + q4R + "                    " + (q4Length / q4R));
-                System.out.println("q5长度                    到达速率                    拥塞指数");
-                System.out.println(q5Length + "                    " + q5R + "                    " + (q5Length / q5R));
+                System.out.println(q4Length + "                    " + q4.getRij1() + "                    " + (q4Length / q4W));
                 System.out.println();
                 eTime = System.currentTimeMillis();
             }
@@ -91,7 +82,6 @@ public class Monitor implements Runnable{
             fw2.close();
             fw3.close();
             fw4.close();
-            fw5.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
